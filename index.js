19f0b2efa2113fe30e68f5ec0b059bc259849396
noam <noamroger120606@gmail.com>
2024-07-04 19:46:30 +0200

    1.0

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 require('dotenv').config({ path: './.env' });
 
 const express = require('express');
-const session = require('express-session');
 const bodyParser = require('body-parser');
-
 const app = express();
-
-const sessionMiddleware = session({ 
-    secret: process.env.SessionSecret,
-    resave: true,
-    saveUninitialized: true,
-});
-app.use(sessionMiddleware);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('trust proxy', true)
 
-
 const loadRoutes = require('./functions/loadRoutes');
 loadRoutes(app);
 
 app.get("*", (req, res) => {
-    res.render('404');
+    res.render('404', { egg: Math.floor(Math.random() * 10) == 5 });
 });
 
 app.listen(process.env.WEBport, () => {
